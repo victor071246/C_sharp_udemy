0fde7a8f7a13b2a74167df74af323944ff265d34
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-11 22:22:21 -0300

    Add files via upload

---
+++ MySolution/MyConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        /*
+        Esta é uma aplicação de teste,
+        sendo usada apenas para fins de teste.
+        Onde estamos testando linhas e
+        blocos de comentários
+        */
+        static void Main(string[] args)
+        {
+            #region Escreve as cinco primeiras mensagems.
+            Console.WriteLine("Primeiro Comando!");
+            Console.WriteLine("Segundo Comando!");
+            Console.WriteLine("Terceiro Comando!");
+            Console.WriteLine("Quarto Comando");
+            Console.WriteLine("Quinto Comando");
+            #endregion
+
+            #region Executa outros comandos
+            Console.WriteLine("Demais Comandos!");
+            Console.WriteLine("Demais Comandos!");
+            Console.WriteLine("Demais Comandos!");
+            Console.WriteLine("Demais Comandos!");
+            Console.WriteLine("Demais Comandos!");
+            #endregion
+        }
+    }
+}

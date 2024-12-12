cb0d40af166672540ff4836aa3f954ba36b93f20
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-11 22:20:42 -0300

    Add files via upload

---
+++ Primeiro/ConsoleAppDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo, Dot Net");
+            Console.WriteLine("Ola Mundo, Dot Net");
+            Console.WriteLine("Ola Mundo, Dot Net");
+            Console.WriteLine("Ola Mundo, Dot Net");
+            Console.WriteLine("Ola Mundo, Dot Net");
+            Console.WriteLine("Ola Mundo, Dot Net"); 
+        }
+    }
+}
---
+++ Primeiro/ConsoleAppNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleAppNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8874e59c-5fab-49b9-9da1-67443faaf307")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

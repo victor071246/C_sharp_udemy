744200dde83c1502e0187b0b762950f90813620c
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-12 23:41:25 -0300

    Add files via upload

--- Variaveis/Variaveis/Program.cs
+++ Variaveis/Variaveis/Program.cs
@@ -8,6 +8,19 @@ namespace Variaveis
 {
     internal class Program
     {
+        enum Notas
+        {
+            Minimo = 10, 
+            Media = 15,
+            Maxima = 20
+        }
+
+        struct Pessoa
+        {
+            public string nome;
+            public int idade;
+            public double altura;
+        }
         static void Main(string[] args)
         {
             #region Numericas Integrais
@@ -39,9 +52,69 @@ namespace Variaveis
             //valor = real1;
             #endregion
 
-            char letra = 'c';
+            #region Caracteres
+            //char letra = '\u0041';
+            //char escape = '\n';
+            //char literal = 'C';
+            #endregion
 
-            Console.WriteLine(letra);
+            #region Boolean
+            //bool verificar = false;
+            //verificar = true;
+            #endregion
+
+            #region String (Cadeia de Caracteres)
+            //string texto = @"Gabriel Artigas\n 1985 @@@??$$";
+            //string mensagem = null;
+            //mensagem = texto;
+            #endregion
+
+            #region Var
+            //var valor = 100;
+            //valor = 20;
+            //valor = "Gabriel";
+            #endregion
+
+            #region Object (Base para todos os tipos)
+            //object obj = false;
+            //obj = 200;
+            //obj = "Gabriel";
+            #endregion
+
+            #region Constantes
+            //const double pi = 3.1415;
+            //const string nome = "Gabriel";
+            #endregion
+
+            #region Enumeração
+            //Notas notasAlunos = Notas.Media;
+            #endregion
+
+            Pessoa p1 = new Pessoa();
+            p1.altura = 1.65;
+            p1.idade = 35;
+            p1.nome = "Gabriel";
+
+            Pessoa p2 = new Pessoa()
+            {
+                nome = "Arthur",
+                idade = 9,
+                altura = 1.35
+            };
+
+            p1.nome = "Logan";
+
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.idade);
+            Console.WriteLine(p1.altura);
+
+            Console.WriteLine();
+
+            Console.WriteLine(p2.nome);
+            Console.WriteLine(p2.idade);
+            Console.WriteLine(p2.altura);
+
             Console.ReadKey();
         }
     }

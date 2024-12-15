2dba8104395b5b8366e82c7d6b31950d0de6966d
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-14 23:40:10 -0300

    Add files via upload

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operadores matemáticos
+            //int num1 = 10;
+            //int num2 = 2;
+            ////int resultado = num1 + num2;
+
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 % num2); // Retorna o resto da divisão
+            #endregion
+
+            #region Precedência de Operadores
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
+
+            //double res = (num1 + num2) * num3 /  (num3 / 3);
+            #endregion
+
+            int num1 = 10;
+
+            num1 = num1 + 1;
+
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}

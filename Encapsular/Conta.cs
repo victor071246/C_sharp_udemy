85abbd63015f4e901f3a88ecc06cb43021b348f4
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-18 23:25:15 -0300

    Add files via upload

---
+++ Encapsular/Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsular
+{
+    internal class Conta
+    {
+        public string _cliente;
+        private double _saldo;
+        private double _taxa;
+
+        public string Cliente
+        {
+            get { return _cliente; }
+            set
+            {
+                if (value != "Gabriel" && value != "Logan")
+                {
+                    _cliente = "Visitante";
+                    Saldo = 0;
+                }
+                else
+                {
+                    _cliente = value;
+                    Saldo = 100;
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            
+            set
+            {
+                _saldo = value;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            valor += _taxa;
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+    }
+}
---
+++ Heranca/Bicicleta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal sealed class Bicicleta : Veiculo
+    {
+        public void Pedalar()
+        {
+
+        }
+
+        public override void Acelerar()
+        {
+            Console.WriteLine("Acelerou a bicicleta!");
+        }
+        public override void Parar()
+        {
+            Console.WriteLine("Parou a bicicleta!");
+        }
+    }
+}

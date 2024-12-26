c440809fdd1ff8855d76b4749542e39e22c16847
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-12-25 23:54:32 -0300

    Add files via upload

--- ManipularString/ManipularString/Form1.cs
+++ ManipularString/ManipularString/Form1.cs
@@ -48,11 +48,104 @@ namespace ManipularString
             //label1.Text = nome.ToLower();
             #endregion
 
-            string nome = "Gabriel Artigas";
+            #region IndexOf / LastIndexOf
+            //string nome = "Gabriel Artigas";
 
-            int indice = nome.IndexOf("iel");
+            ////int indice = nome.IndexOf('a', 5, 4); 
+            //int indice = nome.LastIndexOf("Artigas");
 
-            label1.Text = "Índice: " + indice;
+            //label1.Text = "Índice: " + indice;
+            #endregion
+
+            #region Insert / Replace
+            //string nome = "Gabriel Artigas";
+
+            ////string nomeFinal = nome.Insert(7, " Lucas ");
+
+            //string nomeFinal = nome.Replace("Artigas", "Silva");
+
+            //label1.Text = nomeFinal;
+            #endregion
+
+            #region Length / Substring
+            //string nome = "Gabriel Artigas";
+
+            //int tamanho = nome.Length;
+
+            //label1.Text = "O nome " + nome + " contém " + tamanho + " letras";
+
+            //for (int i = 0; i < nome.Length; i++)
+            //{
+            //    label1.Text += nome[i] + "\n";
+            //}
+
+            //string parte = nome.Substring(nome.IndexOf(" "), nome.Length);
+
+            //label1.Text = parte;
+            #endregion
+
+            #region Split
+
+            //string nomes = "Gabriel_+_Danny Arthur Glória Bia Flávio";
+
+            ////char[] separador = { ' ', '-', '_' };
+
+            ////string[] resultado = nomes.Split(separador);
+
+            //string[] separador = { " ", "_ + _" };
+
+            //string[] resultado = nomes.Split(separador, StringSplitOptions.None);
+
+            //foreach (string nome in resultado)
+            //{
+            //    label1.Text += nome + "\n";
+            //}
+            #endregion
+
+            #region StartsWith / EndsWith
+            //string nome = "Gabriel";
+
+            ////bool res = nome.StartsWith("gab", StringComparison.OrdinalIgnoreCase);
+
+            //bool res = nome.EndsWith("EL", StringComparison.OrdinalIgnoreCase);
+
+            //if (res)
+            //{
+            //    label1.Text = "Positivo.";
+            //}
+            //else
+            //{
+            //    label1.Text = "Negativo.";
+            //}
+            #endregion
+
+            #region Trim / TrimStart / TrimEnd
+            //string mensagem = "    Ola Gabriel   ";
+
+            //string mensagem = "    -_ 5  Ola Gabriel  --__    ";
+            //char[] c = { ' ', '-', '_', '5' };
+            //string nova = mensagem.Trim(c);
+            //mensagem.TrimStart()
+
+            //label1.Text = ">" + mensagem.TrimEnd() + "<";
+            #endregion
+
+            #region Equals / CompareTo
+            //string nome = "Lucas";
+
+            //string nome2 = "Gabriel";
+
+            //if (nome.Equals(nome2, StringComparison.OrdinalIgnoreCase))
+            //{
+            //    label1.Text = "Positivo";
+            //}
+            //else
+            //{
+            //    label1.Text = "Negativo";
+            //}
+
+            //label1.Text = nome.CompareTo(nome2).ToString(); // retorna um inteiro de acordo com a ordem alfabética, -1 se vem depois e 1 se vem antes
+            #endregion
         }
     }
 }

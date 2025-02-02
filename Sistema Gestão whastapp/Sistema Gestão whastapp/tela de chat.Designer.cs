d72f0673fbd5f079ceedeb986538dfcd33e7dede
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2025-02-01 23:07:42 -0300

    Add files via upload

---
+++ Sistema Gestão whastapp/Sistema Gestão whastapp/tela de chat.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Sistema_Gestão_whastapp
+{
+    partial class tela_de_chat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(-5, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 610);
+            this.panel1.TabIndex = 0;
+            // 
+            // tela_de_chat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 607);
+            this.Controls.Add(this.panel1);
+            this.Name = "tela_de_chat";
+            this.Text = "tela_de_chat";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}

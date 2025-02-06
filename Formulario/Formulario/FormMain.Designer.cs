d204a5687d63bd53c6ac4099b739294902f0b7e3
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2025-02-05 23:54:17 -0300

    Add files via upload

---
+++ Formulario/Formulario/FormMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Formulario
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnSegunda = new System.Windows.Forms.Button();
+            this.btnSegundaFormThread = new System.Windows.Forms.Button();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mFileNovo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mFileAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mFileSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mHelpSobre = new System.Windows.Forms.ToolStripMenuItem();
+            this.mHelpSobreDes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mHelpSobreVersao = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboMenu = new System.Windows.Forms.ToolStripComboBox();
+            this.mPesquisar = new System.Windows.Forms.ToolStripTextBox();
+            this.menuBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitulo.Location = new System.Drawing.Point(12, 54);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(929, 178);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Principal";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSegunda
+            // 
+            this.btnSegunda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegunda.Location = new System.Drawing.Point(30, 441);
+            this.btnSegunda.Name = "btnSegunda";
+            this.btnSegunda.Size = new System.Drawing.Size(255, 58);
+            this.btnSegunda.TabIndex = 1;
+            this.btnSegunda.Text = "Segunda Form";
+            this.btnSegunda.UseVisualStyleBackColor = true;
+            this.btnSegunda.Click += new System.EventHandler(this.btnSegunda_Click);
+            // 
+            // btnSegundaFormThread
+            // 
+            this.btnSegundaFormThread.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegundaFormThread.Location = new System.Drawing.Point(291, 441);
+            this.btnSegundaFormThread.Name = "btnSegundaFormThread";
+            this.btnSegundaFormThread.Size = new System.Drawing.Size(255, 58);
+            this.btnSegundaFormThread.TabIndex = 2;
+            this.btnSegundaFormThread.Text = "Segunda Form Thread";
+            this.btnSegundaFormThread.UseVisualStyleBackColor = true;
+            this.btnSegundaFormThread.Click += new System.EventHandler(this.btnSegundaFormThread_Click);
+            // 
+            // menuBar
+            // 
+            this.menuBar.BackColor = System.Drawing.SystemColors.Control;
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.menuHelp,
+            this.comboMenu,
+            this.mPesquisar});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(954, 27);
+            this.menuBar.TabIndex = 3;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mFileNovo,
+            this.mFileAbrir,
+            this.toolStripSeparator1,
+            this.mFileSair});
+            this.menuFile.Name = "menuFile";
+            this.menuFile.Size = new System.Drawing.Size(37, 23);
+            this.menuFile.Text = "File";
+            // 
+            // mFileNovo
+            // 
+            this.mFileNovo.Name = "mFileNovo";
+            this.mFileNovo.Size = new System.Drawing.Size(180, 22);
+            this.mFileNovo.Text = "Novo";
+            this.mFileNovo.Click += new System.EventHandler(this.mFileNovo_Click);
+            // 
+            // mFileAbrir
+            // 
+            this.mFileAbrir.Name = "mFileAbrir";
+            this.mFileAbrir.Size = new System.Drawing.Size(180, 22);
+            this.mFileAbrir.Text = "Abrir";
+            this.mFileAbrir.Click += new System.EventHandler(this.mFileAbrir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mFileSair
+            // 
+            this.mFileSair.Name = "mFileSair";
+            this.mFileSair.Size = new System.Drawing.Size(180, 22);
+            this.mFileSair.Text = "Sair";
+            this.mFileSair.Click += new System.EventHandler(this.mFileSair_Click);
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mHelpSobre});
+            this.menuHelp.Name = "menuHelp";
+            this.menuHelp.Size = new System.Drawing.Size(44, 23);
+            this.menuHelp.Text = "Help";
+            // 
+            // mHelpSobre
+            // 
+            this.mHelpSobre.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mHelpSobreDes,
+            this.mHelpSobreVersao});
+            this.mHelpSobre.Name = "mHelpSobre";
+            this.mHelpSobre.Size = new System.Drawing.Size(180, 22);
+            this.mHelpSobre.Text = "Sobre";
+            // 
+            // mHelpSobreDes
+            // 
+            this.mHelpSobreDes.Name = "mHelpSobreDes";
+            this.mHelpSobreDes.Size = new System.Drawing.Size(180, 22);
+            this.mHelpSobreDes.Text = "Desenvolvedor";
+            this.mHelpSobreDes.Click += new System.EventHandler(this.mHelpSobreDes_Click);
+            // 
+            // mHelpSobreVersao
+            // 
+            this.mHelpSobreVersao.Name = "mHelpSobreVersao";
+            this.mHelpSobreVersao.Size = new System.Drawing.Size(180, 22);
+            this.mHelpSobreVersao.Text = "Versão";
+            this.mHelpSobreVersao.Click += new System.EventHandler(this.mHelpSobreVersao_Click);
+            // 
+            // comboMenu
+            // 
+            this.comboMenu.Items.AddRange(new object[] {
+            "Inglês",
+            "Português"});
+            this.comboMenu.Name = "comboMenu";
+            this.comboMenu.Size = new System.Drawing.Size(121, 23);
+            this.comboMenu.SelectedIndexChanged += new System.EventHandler(this.comboMenu_SelectedIndexChanged);
+            // 
+            // mPesquisar
+            // 
+            this.mPesquisar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.mPesquisar.Name = "mPesquisar";
+            this.mPesquisar.Size = new System.Drawing.Size(200, 23);
+            this.mPesquisar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Pesquisar_KeyUp);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 531);
+            this.Controls.Add(this.btnSegundaFormThread);
+            this.Controls.Add(this.btnSegunda);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.menuBar);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnSegunda;
+        private System.Windows.Forms.Button btnSegundaFormThread;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem menuFile;
+        private System.Windows.Forms.ToolStripMenuItem mFileNovo;
+        private System.Windows.Forms.ToolStripMenuItem mFileAbrir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mFileSair;
+        private System.Windows.Forms.ToolStripMenuItem menuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mHelpSobre;
+        private System.Windows.Forms.ToolStripMenuItem mHelpSobreDes;
+        private System.Windows.Forms.ToolStripMenuItem mHelpSobreVersao;
+        private System.Windows.Forms.ToolStripComboBox comboMenu;
+        private System.Windows.Forms.ToolStripTextBox mPesquisar;
+    }
+}
+
---
+++ Formulario/Formulario/FormSegunda.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Formulario
+{
+    partial class FormSegunda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPrincipal = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnRetorno = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Segunda Form";
+            // 
+            // btnPrincipal
+            // 
+            this.btnPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrincipal.Location = new System.Drawing.Point(551, 373);
+            this.btnPrincipal.Name = "btnPrincipal";
+            this.btnPrincipal.Size = new System.Drawing.Size(237, 65);
+            this.btnPrincipal.TabIndex = 1;
+            this.btnPrincipal.Text = "Principal";
+            this.btnPrincipal.UseVisualStyleBackColor = true;
+            this.btnPrincipal.Click += new System.EventHandler(this.btnPrincipal_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(104, 246);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(530, 44);
+            this.txtMensagem.TabIndex = 2;
+            // 
+            // btnRetorno
+            // 
+            this.btnRetorno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetorno.Location = new System.Drawing.Point(286, 373);
+            this.btnRetorno.Name = "btnRetorno";
+            this.btnRetorno.Size = new System.Drawing.Size(237, 65);
+            this.btnRetorno.TabIndex = 3;
+            this.btnRetorno.Text = "Retorno";
+            this.btnRetorno.UseVisualStyleBackColor = true;
+            this.btnRetorno.Click += new System.EventHandler(this.btnRetorno_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(12, 373);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(237, 65);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // FormSegunda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnRetorno);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.btnPrincipal);
+            this.Controls.Add(this.label1);
+            this.Name = "FormSegunda";
+            this.Text = "FormSegunda";
+            this.Load += new System.EventHandler(this.FormSegunda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPrincipal;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnRetorno;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
